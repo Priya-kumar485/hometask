eb332083bdfbee90f8d1c3660eb805ddcf23c509
Priya-kumar485 <60169518+Priya-kumar485@users.noreply.github.com>
2020-02-09 11:55:16 +0530

    Add files via upload

---
+++ Mul.java
@@ -0,0 +1,9 @@
+package epamtask3;
+
+
+public class Mul extends Operations{
+	public int op(int a, int b){
+	       int mul = a*b;
+	       return mul;
+	}
+}
---
+++ Sub.java
@@ -0,0 +1,9 @@
+package epamtask3;
+
+
+public class Sub extends Operations{
+	public int op(int a, int b){
+	       int sub = a-b;
+	       return sub;
+	}
+}
